1e85553132fffa4424a81589cd31e8178858dfc6
Prashanthn7 <pn342604@gmail.com>
2025-01-07 15:06:05 +0530

    Portfolioupdate1

---
+++ index.js
@@ -0,0 +1,44 @@
+// console.log('Hello world')
+
+// alert('hello world')
+
+// document.write('hello world');
+
+// document.write('<h1>HELLO</h1>')
+
+// let isConfirm = confirm('Are you sure');
+// console.log(isConfirm);
+
+// let a = prompt('Enter first number')
+// let b = prompt('enter the seconf number')
+
+// let sum = a - (-b)
+// console.log(sum);
+
+// for(let a = 1;a<=10;a++){
+//     console.log(a);
+// }
+
+// let cont = document.getElementById('summary')
+// console.log(cont)
+// console.log(cont.innerText)
+
+// let paras = document.getElementsByClassName('para')
+// console.log(paras)
+// console.log(paras[1].innerHTML)
+// console.log(paras)
+
+
+// let users=['abhay','bharath','chethan','divakar','eshwar','pavan']
+
+// let list = document.getElementById('list')
+// let h1 = document.createElement('h1')
+// h1.innerText=`this is the list of ${users.length} users`
+// list.appendChild(h1)
+
+// for(let i = 0;i<users.length;i++){
+//     let li= document.createElement('li')
+//     li.innerText=users[i]
+//     list.appendChild(li)
+// }
+
